49a8d89d6862416238629bf7fe9d8dd844f0b35c
alaoc <ala.ochman@gmail.com>
2017-11-25 13:30:01 +0100

    primeNumberGenerator

--- src/com/company/Monster.java
+++ src/com/company/Monster.java
@@ -1,6 +1,13 @@
 package com.company;
+import java.util.ArrayList;
 import java.util.Random;
 
+
+/**
+ * A class of Monsters
+ *
+ * @author Boris Shulov & Alicja Ochman
+ */
 public class Monster {
 
     public final String name;
@@ -38,5 +45,40 @@ public class Monster {
 
     public String getName(){
         return this.name;
+    }
+
+    int MAX_PROTECTION = 40;
+    int MIN_PROTECTION = 1;
+
+    public ArrayList<Integer> primeList(int MAX_PROTECTION) {
+        ArrayList<Integer> primeList = new ArrayList<Integer>();
+        for(int i=1; i < MAX_PROTECTION; i++){
+            boolean isPrime = true;
+            for(int j=2; j < i; j++ ){
+                if(i % j ==0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime)
+                primeList.add(i);
+        }
+        return primeList;
+    }
+
+    public boolean isPrime(int n) {
+        //check if n is a multiple of 2
+        if (n%2==0) return false;
+        //if not, then just check the odds
+        for(int i=3;i*i<=n;i+=2) {
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+
+    public void setProtection(){
+        Random protection = new Random();
+
     }
 }

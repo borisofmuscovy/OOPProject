e5fb271fe370f0752e69b81f19f132a2505c6687
Sargonos <21023619+borisofmuscovy@users.noreply.github.com>
2017-12-04 14:15:20 +0100

    tests and some minor stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,21 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
+        Monster Bob = new Monster("Bob", 40);
+        Monster Alice = new Monster("Alice", 40);
 
-        Monster Bob = new Monster("Bob", 15);
-        Monster Alice = new Monster("Alice", 15);
-        while (Bob.isAlive() && Alice.isAlive()) {
-            Bob.hit(Alice);
-            Alice.hit(Bob);
-        }
+        if (Math.random() < 0.5){
+            while (Bob.isAlive() && Alice.isAlive()) {
+                Bob.hit(Alice);
+                Alice.hit(Bob);
+            }
+        } else {
+                while (Bob.isAlive() && Alice.isAlive()) {
+                    Alice.hit(Bob);
+                    Bob.hit(Alice);
+                }
+            }
+
         if (Bob.isAlive() && Bob.isAlive()) {
             System.out.println(Bob.getName() + " has Protection " + Bob.getProtection() + ", Strength " +
                     Bob.getStrength() + ", Damage " + Bob.getDamage() + " and their maximum HP is " + Bob.getMAX_HP() + ".");

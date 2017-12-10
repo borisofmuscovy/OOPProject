d72cc56fc81f0c16c28e62056cab10049465e2b6
alaoc <ala.ochman@gmail.com>
2017-12-10 18:02:06 +0100

    Weapon: ID, weight, Damage, nominal Value Purses: ID , Content, TotalValue, Capacity

--- src/com/company/Purse.java
+++ src/com/company/Purse.java
@@ -1,4 +1,121 @@
 package com.company;
 
-public class Purse {
+import java.util.Random;
+
+public class Purse extends InventoryItem {
+    int content;
+    final int capacity;
+    int totalValue;
+    /**
+     * Constructor of purse object
+     * Value of intialized content cannot be bigger than purses capacity
+     * @param value
+     *        Sets value of the purse
+     * @param holder
+     *        Monster that carries the purse
+     * @param content
+     *        Sets content of the purse at given time
+     * @param capacity
+     *        Describes maximum capacity of the purse
+     */
+    public Purse(int value, Monster holder, int content, int capacity){
+        super(value, holder);
+        setPurseID(25);
+        assert(content <= capacity);
+        this.content = content;
+        this.capacity = capacity;
+    }
+
+    /**
+     * Fibonacci series generator
+     * @param   number
+     *          Number indicating how many fibonacci numbers are going to be generated
+     * @return  long[] fibonacciSeries
+     *          Array containing fibonacci numbers of type long
+     */
+    public static int[] fibonacci(int number) {
+        int[] fibonacciSeries = new int[number];
+        fibonacciSeries[0] = 0;
+        fibonacciSeries[1] = 1;
+        for(int i = 2; i < number; i++){
+            fibonacciSeries[i] = fibonacciSeries[i - 1] + fibonacciSeries [i - 2];
+        }
+        return fibonacciSeries;
+    }
+
+    /**
+     *  Method returning random long number from fibonacci series
+     * @param   n
+     *          Number of fibonacci number to generate
+     * @return  purseID
+     *          Random long from the fibonacciSeries array
+     */
+    public long setPurseID(int n){
+        int[] fibonacciSeries = fibonacci(n);
+        Random r = new Random();
+        long purseID = (long) fibonacciSeries[r.nextInt(fibonacciSeries.length)];
+        return purseID;
+    }
+
+    /**
+     * Sets new value of the content of a purse
+     * @param newContent
+     *        Describes new content of a purse
+     */
+    public void setContent(int newContent){
+        this.content = newContent;
+    }
+
+    /**
+     * Adds content to the purse
+     * If content of the purse exceeds its capacity, the purse is torn
+     * @param dukats
+     *        Number of dukats to be added to the purse
+     */
+    public void addContent(int dukats){
+        setContent(this.content + dukats);
+        if(this.content > this.capacity)
+            tearThePurse();
+    }
+
+    /**
+     * Removes dukats from the purse
+     * @param dukats
+     *        Number of dukats to be removed from the purse
+     */
+    public void removeContent(int dukats){
+        setContent(this.content - dukats);
+    }
+
+    /**
+     * Transfers money from one purse to the other
+     * If the new content of one of the pursee exceeds its capacity, the purse is torn
+     * @param other
+     *        Other purse
+     * @param dukats
+     *        Number of dukats to be transferred
+     */
+    public void transferContenr(Purse other, int dukats){
+        setContent(this.content - dukats);
+        setContent(other.content + dukats);
+        if(this.content > this.capacity)
+            this.tearThePurse();
+        if(other.content > other.capacity)
+            other.tearThePurse();
+    }
+
+    /**
+     * Calculates total value of the purse
+     */
+    public void totalValue(){
+        this.totalValue = this.value + this.content;
+    }
+
+    /**
+     * Method to destroy the purse
+     */
+    public void tearThePurse(){
+        this.value = 0;
+        this.content = 0;
+    }
 }
--- src/com/company/Weapon.java
+++ src/com/company/Weapon.java
@@ -1,4 +1,83 @@
 package com.company;
 
-public class Weapon {
+import java.util.List;
+import java.util.Random;
+
+public class Weapon extends InventoryItem {
+    int damage;
+    int MAX_DAMAGE = 20;
+    List allIDs;
+
+    /**
+     * Constructor of weapon object
+     * @param weight
+     *        Describes weight of the weapon
+     * @param value
+     *        Describes value of the weapon
+     * @param holder
+     *        Monster that holds the weapon
+     */
+    public Weapon(float weight, int value, Monster holder){
+        super(value, holder);
+        this.damage = setDamage(MAX_DAMAGE);
+        this.weight = setWeight(weight);
+        setWeaponID(allIDs);
+    }
+    /**
+     * Method setting unique, odd ID of a weapon of long type
+     * @param   allIDs
+     *          List of ID's that were ascribed to other weapons
+     * @return  weaponID
+     */
+    public long setWeaponID(List allIDs){
+        long weaponID = 0;
+        int i = 1;
+        while(i == 1){
+            Random ID = new Random();
+            weaponID = ID.nextLong();
+            if((weaponID % 2 != 0) &&(!allIDs.contains(weaponID))){
+                allIDs.add(weaponID);
+                i = 0;
+            }else
+                i =1;
+        }
+        return weaponID;
+    }
+
+    /**
+     * Sets damage to a random number from 1 to MAX_DAMAGE
+     * @param MAX_DAMAGE
+     *        Maximum damage
+     * @return damage
+     */
+    public int setDamage(int MAX_DAMAGE){
+        Random r = new Random();
+        damage = r.nextInt(MAX_DAMAGE + 1) + 1;
+        return damage;
+    }
+
+    /**
+     * Method setting weight of an item
+     * @param   weight
+     *          | Non-negative float
+     * @return  weight
+     *
+     * @throws  IllegalArgumentException
+     *          Thrown if the weight of the weapon is about to be smaller than 0
+     *          | weight < 0
+     */
+    public float setWeight(float weight) throws IllegalArgumentException{
+        if(weight <0)
+            throw new IllegalArgumentException();
+        else
+            return weight;
+    }
+
+    /**
+     * Gets weight of an item
+     * @return  this.weight
+     */
+    public float getWeight(){
+        return this.weight;
+    }
 }

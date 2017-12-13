07046940238803102a0c00629cdb0a4d276065fb
alaoc <ala.ochman@gmail.com>
2017-12-13 14:54:31 +0100

    Inventory constructor and backpack

---
+++ src/com/company/Backpack.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Backpack extends InventoryItem{
+
+    public Backpack(int value, Monster holder, float weight){
+        super(value, holder, weight);
+    }
+}
